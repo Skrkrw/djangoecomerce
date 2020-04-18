88959250674ef4525b8e9656dae95b428f1d9d01
Jose Enciso <josejefloen@gmail.com>
2020-04-18 13:31:18 +0000

    Productions Views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    products = Product.object.all() # Return all the products that are in the DB
+    return render(request, "products.html", {"products": products}) # Render products.html and accessing to all products
