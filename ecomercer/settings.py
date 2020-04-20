7d5244c79dec1ef67c0c0e5f9b49fb5b1fc088e7
Jose Enciso <josejefloen@gmail.com>
2020-04-20 19:41:59 +0000

    Cart app Done

--- ecomercer/settings.py
+++ ecomercer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,7 +71,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media', # After this line we need a MEDIA_ROOT
-                'cart_contexts,cart_contents', # context_processors are  a list of things available on a webpage
+                'cart.contexts.cart_contents', # context_processors are  a list of things available on a webpage
             ],
         },
     },
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # All medias 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
