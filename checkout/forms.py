1195ae859ac3cb622fa084db8877de35f39effd3
Jose Enciso <josejefloen@gmail.com>
2020-04-23 21:36:13 +0000

    Sorting checkout forms out

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,13 +3,13 @@ from .models import Order
 
 class MakePaymentForm(forms.Form):
     # Month and YEAR Expire
-    MONTH_CHOICE = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICE = [(i, i) for i in range(2017, 2036)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
     
     credit_card_number = forms.CharField(label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICE)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICE, required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 # For the customer to purchase products
